971c412fe9b4e1afc24509d4445977a7002a30e3
nsa2-argocd <devops@company.com>
2025-07-14 12:59:28 -0700

    Update service-foundry-app-frontend chart at Mon Jul 14 12:59:28 PDT 2025

--- push-to-github.sh
+++ push-to-github.sh
@@ -4,6 +4,7 @@ set -e
 all_charts=(
     "service-foundry-builder"
     "react-o11y-app"
+    "service-foundry-app-frontend"
 )
 
 usage() {
@@ -22,13 +23,13 @@ usage() {
 
 GITHUB_USERNAME="${GITHUB_USERNAME:-}"
 GITHUB_PASSWORD="${GITHUB_PASSWORD:-}"
-TAG=$(cat current-version.txt)
-while getopts "hu:p:t:" OPTION
+#TAG=$(cat current-version.txt)
+while getopts "hu:p:" OPTION
 do
   case ${OPTION} in
-    u) GITHUB_USERNAME="${OPTARG}" ;;
-    p) GITHUB_PASSWORD="${OPTARG}" ;;
-    t) TAG="${OPTARG}" ;;
+#    u) GITHUB_USERNAME="${OPTARG}" ;;
+#    p) GITHUB_PASSWORD="${OPTARG}" ;;
+#    t) TAG="${OPTARG}" ;;
     h) usage ;;
     ?) usage ;;
   esac
@@ -36,33 +37,35 @@ done
 
 
 # check all arguments are provided
-if [ -z "${GITHUB_USERNAME}" ] || [ -z "${GITHUB_PASSWORD}" ] || [ -z "${TAG}" ]; then
-  echo "All arguments are required"
-  usage
-fi
+#if [ -z "${GITHUB_USERNAME}" ] || [ -z "${GITHUB_PASSWORD}" ] ; then
+#  echo "All arguments are required"
+#  usage
+#fi
 
 echo "====== INFO ======"
 echo "Make sure you have the latest version of the repository checked out."
-echo "react-o11y-app/push-to-dockerhub.sh -t ${TAG} -u github_username -p github_password"
-echo "service-foundry-builder/push-to-dockerhub.sh -t ${TAG} -u github_username -p github_password"
+echo "react-o11y-app/push-to-dockerhub.sh -u github_username -p github_password"
+echo "service-foundry-builder/push-to-dockerhub.sh -u github_username -p github_password"
 echo "==================="
 
-CHART_VERSION="${TAG}"
+#CHART_VERSION="${TAG}"
 #REPOSITORY="credemol/service-foundry-builder"
 
 for chart in "${all_charts[@]}"; do
   REPOSITORY="credemol/${chart}"
-  echo "Updating chart version for ${chart} to ${TAG}"
+  echo "Updating chart version for ${chart}"
+#  echo "Updating chart version for ${chart} to ${TAG}"
+  # update the chart version and repository in Chart.yaml and values.yaml manually
   # if OS is MacOS
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/version:.*/version: ${TAG}/" "${chart}/Chart.yaml"
-    set -i '' "s/repository:.*/repository: ${REPOSITORY}/" "${chart}/values.yaml"
-    sed -i '' "s/tag:.*/tag: ${TAG}/" "${chart}/values.yaml"
-  else
-    sed -i "s/version:.*/version: ${TAG}/" "${chart}/Chart.yaml"
-    sed -i "s/repository:.*/repository: ${REPOSITORY}/" "${chart}/values.yaml"
-    sed -i "s/tag:.*/tag: ${TAG}/" "${chart}/values.yaml"
-  fi
+#  if [[ "$OSTYPE" == "darwin"* ]]; then
+#    sed -i '' "s/version:.*/version: ${TAG}/" "${chart}/Chart.yaml"
+#    set -i '' "s/repository:.*/repository: ${REPOSITORY}/" "${chart}/values.yaml"
+#    sed -i '' "s/tag:.*/tag: ${TAG}/" "${chart}/values.yaml"
+#  else
+#    sed -i "s/version:.*/version: ${TAG}/" "${chart}/Chart.yaml"
+#    sed -i "s/repository:.*/repository: ${REPOSITORY}/" "${chart}/values.yaml"
+#    sed -i "s/tag:.*/tag: ${TAG}/" "${chart}/values.yaml"
+#  fi
 
   helm package ${chart}
 done
@@ -70,11 +73,12 @@ done
 helm repo index . --url https://nsalexamy.github.io/helm-charts
 
 git add .
-git commit -m "Update ${chart} chart to version $CHART_VERSION"
-
-git tag -a v${CHART_VERSION} -m "Release version $CHART_VERSION"
+git commit -m "Update ${chart} chart at $(date)"
 
 git push origin main
-git push origin v${CHART_VERSION}
+
+# If needed, create a new tag for the release manually
+#git tag -a v${CHART_VERSION} -m "Release version $CHART_VERSION"
+#git push origin v${CHART_VERSION}
 
 echo "Merge the changes of main branch to the ph-pages branch on Github "
